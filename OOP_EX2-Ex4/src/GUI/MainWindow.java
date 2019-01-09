f4642ae5281e1caeaf26d83c286d1e9684e310f6
Roi Abramovitch <roiabr15@gmai.com>
2019-01-09 13:08:22 +0100

    gui

--- OOP_EX2-Ex4/src/GUI/MainWindow.java
+++ OOP_EX2-Ex4/src/GUI/MainWindow.java
@@ -222,7 +222,7 @@ public class MainWindow extends JFrame implements MouseListener
 		GuiGame = new Game(play1);
 		try {
 
-			Thread.sleep(100);
+			Thread.sleep(10);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
